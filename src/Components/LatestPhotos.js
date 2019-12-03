ce1fcb8670249b72f0efa2f743bc6a9840a2e90d
engineer-aslam-hossain <engineer.aslamhossain@gmail.com>
2019-12-03 14:30:33 +0600

    modify search button

--- src/Components/LatestPhotos.js
+++ src/Components/LatestPhotos.js
@@ -53,13 +53,53 @@ export default class LatestPhotos extends Component {
                     photos : res.data.results,
                     loading : false,
                     searching : true,
-                    page: this.state.page +1
+                    page: 2,
+                    total_found : res.data.total,
+                    total_found_pages : res.data.total_pages
             })  
         ) 
         e.preventDefault()
 }
 
+loadNextSearchPage = (e) => {
+
+    axios.get('https://api.unsplash.com/search/photos/?client_id=f13a3cd52d95b1a9c5484d76ad583276ad8de31fece56dd46177c066f6528752&query='+this.state.search_query+'&per_page=16&page='+this.state.page).then(
+            res => this.setState({
+                    photos : res.data.results,
+                    loading : false,
+                    searching : true,
+                    page: this.state.page +1,
+                    total_found : res.data.total,
+                    total_found_pages : res.data.total_pages
+            })  
+        ) 
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })       
+}
+searchQuery = (e) => {
+        this.setState({
+            search_query : e.target.value,
+        })
+      
+}
+
     render() {
+
+        let searchHeading = ''
+        let searchbtnMarkup = ''
+        let searchinfo = ''
+
+        if(this.state.searching ===true){
+                searchHeading = <h2>You Searched With :  <i>{this.state.search_query} </i></h2>
+                searchbtnMarkup = <button className="btn btn-success" onClick={this.loadNextSearchPage} >Load Page    {this.state.page } </button>
+                searchinfo =  <span>Total Found {this.state.total_found } |  Page {this.state.page -1} of {this.state.total_found_pages} </span>
+        }else{
+                searchHeading= <h2>Latest Photos </h2>
+                searchbtnMarkup = <button className="btn btn-success" onClick={this.loadNextPage} >Load Page    {this.state.page } </button>    
+        }
+
         if(this.state.loading === true){
             return (
                 <div className="row text-center" ><div className="col">Loading Photos....</div> </div>
@@ -69,7 +109,7 @@ export default class LatestPhotos extends Component {
         return (
             <React.Fragment>
                 <div className="row top-heading">
-                    <div className="col my-auto"><h2>Latest Photos </h2></div>
+                    <div className="col my-auto">{searchHeading}{searchinfo} </div>
                     <div className="col col-auto my-auto">
                       <form onSubmit={this.searchTrigger} action="">
                                 <input type="text" value={this.state.search_query} onChange={this.searchQuery} placeholder="Search Keyword" />
@@ -98,7 +138,7 @@ export default class LatestPhotos extends Component {
               <div className="row">
               <div className="col-lg-12 text-center">
                   <div className="load-more-btn">
-                      <button className="btn btn-success" onClick={this.loadNextPage} >Load Page {this.state.page } </button>
+                      {searchbtnMarkup}
                   </div>
               </div>
               </div>
